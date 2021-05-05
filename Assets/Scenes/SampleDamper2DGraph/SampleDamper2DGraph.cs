2241a5a84778fa3ac41bf3ac07f4f0d894243f6a
Meta <bacota@gmail.com>
2021-05-05 14:34:15 +0200

    ArtOfDamping v1
    Code used to write the first version of the article at https://www.alexisbacot.com/blog/the-art-of-damping

---
+++ Assets/Scenes/SampleDamper2DGraph/SampleDamper2DGraph.cs
@@ -0,0 +1,121 @@
+﻿//--------------------------------------------------------------------
+// Created by Alexis Bacot - 2021 - www.alexisbacot.com
+//--------------------------------------------------------------------
+using UnityEngine;
+
+//--------------------------------------------------------------------
+public class SampleDamper2DGraph : MonoBehaviour
+{
+    //--------------------------------------------------------------------
+    public enum EnumDampingType
+    {
+        None,
+        Normal,
+        Spring,
+        UnitySmoothDamp,
+        SpringCritical,
+        SpringCriticalNoGoalSpeed,
+        DoubleCriticalNoGoalSpeed,
+        //TimedCriticalNoGoalSpeed,
+    }
+
+    //--------------------------------------------------------------------
+    [Header("Damping Params")]
+    public EnumDampingType DampingType = EnumDampingType.Spring;
+    public float halflife = 0.5f;
+    public float frequency = 0.5f;
+    public float speedMoveRight = 20.0f;
+
+    //[Header("Timed Damping Params")]
+    //public float timeToGoal = 1.0f;
+    //public float apprehension = 2.0f;
+
+    [Header("Goals Params")]
+    public float goalMinY;
+    public float goalMaxY, goalMoveSpeed;
+
+    [Header("Links")]
+    public Transform tsfmGoal;
+    public Transform tsfmFollowing;
+    public Camera cam;
+
+    // Internal
+    private Vector3 _posFollower, _posGoal, _posCam;
+    private float _speedFollower;
+    private float _speedGoal;
+    // Another set of position and speed used for the double damper
+    private float posGoal2, speedGoal2;
+
+    //--------------------------------------------------------------------
+    private void Update()
+    {
+        _posGoal = tsfmGoal.position;
+        _posFollower = tsfmFollowing.position;
+        _posCam = cam.transform.position;
+
+        MoveGoal();
+
+        // While changing values in the editor sometimes these values will be 0, we want to skip these!
+        if (halflife == 0 || frequency == 0) return;
+
+        // DAMPING: Follower y pos tries to match goal y
+        switch(DampingType)
+        {
+            case EnumDampingType.Normal: _posFollower.y = ToolDamper.damper(_posFollower.y, _posGoal.y, halflife, Time.deltaTime); break;
+            case EnumDampingType.Spring: ToolDamper.damper_spring(ref _posFollower.y, ref _speedFollower, _posGoal.y, _speedGoal, frequency, halflife, Time.deltaTime); break;
+            case EnumDampingType.UnitySmoothDamp: _posFollower.y = Mathf.SmoothDamp(_posFollower.y, _posGoal.y, ref _speedFollower, halflife); break;
+            case EnumDampingType.SpringCritical: ToolDamper.damper_spring_critical(ref _posFollower.y, ref _speedFollower, _posGoal.y, _speedGoal, halflife, Time.deltaTime); break;
+            case EnumDampingType.SpringCriticalNoGoalSpeed: ToolDamper.damper_spring_critical_noGoalSpeed(ref _posFollower.y, ref _speedFollower, _posGoal.y, halflife, Time.deltaTime); break;
+            case EnumDampingType.DoubleCriticalNoGoalSpeed: ToolDamper.double_spring_damper_implicit(ref _posFollower.y, ref _speedFollower, ref posGoal2, ref speedGoal2, _posGoal.y, halflife, Time.deltaTime); break;
+            //case EnumDampingType.TimedCriticalNoGoalSpeed: ToolDamper.timed_spring_damper_implicit(ref _posFollower.y, ref _speedFollower, ref posGoal2, _posGoal.y, timeToGoal, halflife, Time.deltaTime, apprehension); break;
+            default: Debug.LogWarning("[SampleDamper] DampingType: " + DampingType + " is not supported"); break;
+        }
+
+        // Move all to the right to have some trails behind follower
+        _posFollower.x += speedMoveRight * Time.deltaTime;
+        _posGoal.x += speedMoveRight * Time.deltaTime;
+        _posCam.x += speedMoveRight * Time.deltaTime;
+
+        // Finalize
+        tsfmFollowing.position = _posFollower;
+        tsfmGoal.position = _posGoal;
+        cam.transform.position = _posCam;
+    }
+
+    //--------------------------------------------------------------------
+    private void MoveGoal()
+    {
+        float newGoalY = 0;
+        bool hasGoalChanged = false;
+
+        // Use arrows to slowly change goal y position
+        float inputVertical = Input.GetAxis("Vertical");
+        if (inputVertical != 0)
+        {
+            newGoalY = _posGoal.y + inputVertical * goalMoveSpeed * Time.deltaTime;
+            hasGoalChanged = true;
+        }
+
+        // Click to change goal y position instantly
+        if (Input.GetMouseButtonDown(0))
+        {
+            newGoalY = cam.ScreenToWorldPoint(Input.mousePosition).y;
+            hasGoalChanged = true;
+        }
+
+        if (hasGoalChanged)
+        {
+            newGoalY = Mathf.Clamp(newGoalY, goalMinY, goalMaxY);
+
+            _speedGoal = (newGoalY - _posGoal.y) / Time.deltaTime;
+
+            _posGoal.y = newGoalY;
+        }
+        else
+            _speedGoal = 0.0f;
+    }
+
+    //--------------------------------------------------------------------
+}
+
+//--------------------------------------------------------------------
---
+++ Assets/Scripts/ToolDamper.cs
@@ -0,0 +1,179 @@
+﻿//--------------------------------------------------------------------
+// Ported by Alexis Bacot - 2021 - www.alexisbacot.com
+// From Daniel Holden (https://twitter.com/anorangeduck)
+// Original article http://theorangeduck.com/page/spring-roll-call#damper
+//--------------------------------------------------------------------
+using UnityEngine;
+
+//--------------------------------------------------------------------
+public class ToolDamper
+{
+    //--------------------------------------------------------------------
+    public static float damper(float x, float g, float halflife, float dt)
+    {
+        return Mathf.Lerp(x, g, 1.0f - fast_negexp((0.69314718056f * dt) / (halflife + Mathf.Epsilon)));
+    }
+
+    //--------------------------------------------------------------------
+    private static float fast_negexp(float x)
+    {
+        return 1.0f / (1.0f + x + 0.48f * x * x + 0.235f * x * x * x);
+    }
+
+    //--------------------------------------------------------------------
+    private static float squaref(float x) { return x * x; }
+
+    //--------------------------------------------------------------------
+    /// <summary>
+    /// Damper spring
+    /// </summary>
+    /// <param name="frequency">how many oscillations are done</param>
+    /// <param name="halflife">how quick the goal is reached</param>
+    public static void damper_spring(ref float x, ref float v, float x_goal, float v_goal, float frequency, float halflife, float dt)
+    {
+        float g = x_goal;
+        float q = v_goal;
+        float s = frequency_to_stiffness(frequency);
+        float d = halflife_to_damping(halflife);
+        float c = g + (d * q) / (s + Mathf.Epsilon);
+        float y = d / 2.0f;
+
+        if (Mathf.Abs(s - (d * d) / 4.0f) < Mathf.Epsilon) // Critically Damped
+        {
+            float j0 = x - c;
+            float j1 = v + j0 * y;
+
+            float eydt = fast_negexp(y * dt);
+
+            x = j0 * eydt + dt * j1 * eydt + c;
+            v = -y * j0 * eydt - y * dt * j1 * eydt + j1 * eydt;
+        }
+        else if (s - (d * d) / 4.0f > 0.0) // Under Damped
+        {
+            float w = Mathf.Sqrt(s - (d * d) / 4.0f);
+            float j = Mathf.Sqrt(squaref(v + y * (x - c)) / (w * w + Mathf.Epsilon) + squaref(x - c));
+            float p = Mathf.Atan((v + (x - c) * y) / (-(x - c) * w + Mathf.Epsilon));
+
+            j = (x - c) > 0.0f ? j : -j;
+
+            float eydt = fast_negexp(y * dt);
+
+            x = j * eydt * Mathf.Cos(w * dt + p) + c;
+            v = -y * j * eydt * Mathf.Cos(w * dt + p) - w * j * eydt * Mathf.Sin(w * dt + p);
+        }
+        else if (s - (d * d) / 4.0f < 0.0) // Over Damped
+        {
+            float y0 = (d + Mathf.Sqrt(d * d - 4 * s)) / 2.0f;
+            float y1 = (d - Mathf.Sqrt(d * d - 4 * s)) / 2.0f;
+            float j1 = (c * y0 - x * y0 - v) / (y1 - y0);
+            float j0 = x - j1 - c;
+
+            float ey0dt = fast_negexp(y0 * dt);
+            float ey1dt = fast_negexp(y1 * dt);
+
+            x = j0 * ey0dt + j1 * ey1dt + c;
+            v = -y0 * j0 * ey0dt - y1 * j1 * ey1dt;
+        }
+    }
+
+    //--------------------------------------------------------------------
+    /// <summary>
+    /// Damper spring set to critical (w = 0) spring goes to goal as fast as possible without oscillations, frequency is set by halflife
+    /// </summary>
+    public static void damper_spring_critical(ref float x, ref float v, float x_goal, float v_goal, float halflife, float dt)
+    {
+        float g = x_goal;
+        float q = v_goal;
+        float d = halflife_to_damping(halflife);
+        float c = g + (d * q) / ((d * d) / 4.0f);
+        float y = d / 2.0f;
+        float j0 = x - c;
+        float j1 = v + j0 * y;
+        float eydt = fast_negexp(y * dt);
+
+        x = eydt * (j0 + j1 * dt) + c;
+        v = eydt * (v - j1 * y * dt);
+    }
+
+    //--------------------------------------------------------------------
+    /// <summary>
+    /// Damper spring set to critical AND we assume the goal has no speed, very smooth! smoothness similar to unity smoothdamping
+    /// </summary>
+    public static void damper_spring_critical_noGoalSpeed( ref float x, ref float v, float x_goal, float halflife, float dt)
+    {
+        float y = halflife_to_damping(halflife) / 2.0f;
+        float j0 = x - x_goal;
+        float j1 = v + j0 * y;
+        float eydt = fast_negexp(y * dt);
+
+        x = eydt * (j0 + j1 * dt) + x_goal;
+        v = eydt * (v - j1 * y * dt);
+    }
+
+    //--------------------------------------------------------------------
+    /// <summary>
+    /// Double Damper spring set to critical, we assume goal has no speed
+    /// Both follower and goal are damped, to achieve maximum smoothness
+    /// </summary>
+    public static void double_spring_damper_implicit( ref float x, ref float v, ref float xi, ref float vi, float x_goal, float halflife, float dt)
+    {
+        damper_spring_critical_noGoalSpeed(ref xi, ref vi, x_goal, 0.5f * halflife, dt);
+        damper_spring_critical_noGoalSpeed(ref x, ref v, xi, 0.5f * halflife, dt);
+    }
+
+    //--------------------------------------------------------------------
+    /*public static void timed_spring_damper_implicit( ref float x, ref float v, ref float xi, float x_goal, float t_goal, float halflife, float dt, float apprehension = 2.0f)
+    {
+        float min_time = t_goal > dt ? t_goal : dt;
+
+        float v_goal = (x_goal - xi) / min_time;
+
+        float t_goal_future = dt + apprehension * halflife;
+        float x_goal_future = t_goal_future < t_goal ?
+            xi + v_goal * t_goal_future : x_goal;
+
+        damper_spring_critical_noGoalSpeed(ref x, ref v, x_goal_future, halflife, dt);
+
+        xi += v_goal * dt;
+    }*/
+
+    //--------------------------------------------------------------------
+    public static float critical_halflife(float frequency)
+    {
+        return damping_to_halflife(Mathf.Sqrt(frequency_to_stiffness(frequency) * 4.0f));
+    }
+
+    //--------------------------------------------------------------------
+    public static float critical_frequency(float halflife)
+    {
+        return stiffness_to_frequency(squaref(halflife_to_damping(halflife)) / 4.0f);
+    }
+
+    //--------------------------------------------------------------------
+    private static float frequency_to_stiffness(float frequency)
+    {
+        return squaref(2.0f * Mathf.PI * frequency);
+    }
+
+    //--------------------------------------------------------------------
+    private static float stiffness_to_frequency(float stiffness)
+    {
+        return Mathf.Sqrt(stiffness) / (2.0f * Mathf.PI);
+    }
+
+    //--------------------------------------------------------------------
+    private static float halflife_to_damping(float halflife)
+    {
+        return (4.0f * 0.69314718056f) / (halflife + Mathf.Epsilon);
+    }
+
+    //--------------------------------------------------------------------
+    private static float damping_to_halflife(float damping)
+    {
+        return (4.0f * 0.69314718056f) / (damping + Mathf.Epsilon);
+    }
+
+    //--------------------------------------------------------------------
+}
+
+//--------------------------------------------------------------------
